d361cbc81a5ed83f00facfe75a81d0365201595e
Jen <jenniferanndcb@gmail.com>
2018-05-11 19:04:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 };
 function sayHiToGrandma(string) {
-  if(string.toLowerCase()===string);
-  return (I can\t hear you!)
+  if(string.toLowerCase() === string)
+  return ("I can't hear you!")
+  else if (string.toUpperCase() === string)
+  return 
 }
