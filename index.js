e7d6764ff4ef10f3b5e09a24fdb2e1b229c3999e
Jen <jenniferanndcb@gmail.com>
2018-05-11 19:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function sayHiToGrandma(string) {
   return ("I can't hear you!")
   else if (string.toUpperCase() === string)
   return ("YES INDEED!")
-  else if (string."I love you, Grandma" === string)
+  else if (string === "I love you, Grandma")
   return ("I love you, too")
 };
